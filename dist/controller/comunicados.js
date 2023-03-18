15496c7963796302959ac615d4f2a7103ac571aa
GustavoM <gustavomunoz@multiplica.com>
2023-03-18 09:26:44 -0500

    Creación de consulta

--- dist/controller/comunicados.js
+++ dist/controller/comunicados.js
@@ -10,19 +10,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getComunicados = void 0;
+const sequelize_1 = require("sequelize");
 const { sequelize } = require("sequelize");
 const init_models_1 = require("../models/init-models");
 const models = (0, init_models_1.initModels)(sequelize);
 const getComunicados = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const query = `SELECT tb_comunicados.id, tb_comunicados.fecha, tb_comunicados.asunto, tb_comunicados.linkPDF FROM tb_comunicados LEFT JOIN (tb_pilotos_id_sim RIGHT JOIN tb_sim_plat_codplat ON tb_pilotos_id_sim.idSimCodPlataforma = tb_sim_plat_codplat.id) ON tb_comunicados.idSimulador = tb_sim_plat_codplat.idSimulador WHERE (((tb_pilotos_id_sim.idPiloto)=124 Or (tb_pilotos_id_sim.idPiloto) Is Null) AND ((tb_comunicados.activo)=1 Or (tb_comunicados.activo) Is Null) AND ((tb_pilotos_id_sim.idEstado)=1 Or (tb_pilotos_id_sim.idEstado) Is Null) AND ((tb_comunicados.idSimulador)=1 Or (tb_comunicados.idSimulador) Is Null)) ORDER BY tb_comunicados.id DESC;`;
     try {
-        const comunicados = yield models.tb_comunicados.findAll({
-            where: {
-                activo: 1
-            },
-            order: [
-                ['id', 'DESC'],
-            ]
-        });
+        const comunicados = yield models.tb_comunicados.sequelize.query(query, { type: sequelize_1.QueryTypes.SELECT });
         if (comunicados.length > 0) {
             return res.status(200).json({
                 ok: true,
@@ -37,6 +33,7 @@ const getComunicados = (req, res) => __awaiter(void 0, void 0, void 0, function*
         }
     }
     catch (error) {
+        console.log(error);
         return res.status(500).json({
             ok: false,
             error: error,
